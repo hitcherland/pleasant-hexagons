f9e65a4825c45fb3e3777929be7ac3a3bc8c4549
hitcherland <hitcherland@gmail.com>
2022-10-01 23:32:21 +0100

    Now it's pleasant

--- index.js
+++ index.js
@@ -1,10 +1,170 @@
-const canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
 
-const options = {
-    width: 800,
-    height: 800,
+function hexToPixel(size, q, j, r) {
+    return [size * 1.5 * q, size * j, size * (factor * q + 2 * factor * r)];
 }
+
+function toIsometricPoint(x, y, z) {
+    const X = (x - z) / Math.sqrt(2) + center[0];
+    const Y = (x + 2 * y + z) / Math.sqrt(6) + center[1];
+    return [X, Y];
+}
+
+
+const audioContext = new AudioContext()
+
+class HexBlock {
+    constructor(q, r, s, size, angle, hue) {
+        this.q = q;
+        this.r = r;
+        this.s = s;
+        this.height = size;
+        this.offsetAngle = angle;
+        this.updateSpeed = 7e-5;
+        this.offsetScale = 0.2;
+        this.offsetHeight = this.offsetScale * size * Math.sin(Math.PI * 2 * this.offsetAngle);
+        this.hue = hue;
+        this.size = size;
+        this.doBeep = false;
+        this.glow = false;
+
+        this.matrix1 = new DOMMatrix([1, 0, 0, 1, Math.random(), this.offsetHeight]);
+        this.matrix2 = new DOMMatrix([1, 0.2, 0.2, 1, 0, this.offsetHeight]);
+
+
+        var o = audioContext.createOscillator()
+        this.g = audioContext.createGain()
+        o.connect(this.g)
+        this.g.connect(audioContext.destination)
+        o.start(0)
+        const freqs = [440, 493.9, 554.4, 587.3, 659.3, 740, 830]
+        o.frequency.value = freqs[Math.floor(freqs.length * Math.random())]
+        this.g.gain.setValueAtTime(0, audioContext.currentTime);
+    }
+
+    beep() {
+        this.g.gain.setValueAtTime(0.05, audioContext.currentTime);
+        this.g.gain.exponentialRampToValueAtTime(
+            0.00001, audioContext.currentTime + 2.0
+        )
+    }
+
+    pixelPosition() {
+        const isoPoints = toIsometricPoint(this.q, this.r, this.s);
+        return hexToPixel(this.size, ...isoPoints);
+    }
+
+    compare(block) {
+        const A = this.pixelPosition();
+        const B = block.pixelPosition();
+        
+        if(A[1] != B[1]) {
+            return A[1] - B[1];
+        } else {
+            return A[0] - B[0]
+        }
+    }
+
+    render(ctx) {
+        ctx.save();
+        const [x, y, z] = hexToPixel(this.size / 2, this.q, this.r, this.s);
+
+        const height = this.height + this.offsetHeight;
+
+        const A = -2 * Math.PI / 6;
+        const [X, Y] = toIsometricPoint(x + 0.5 * this.size * Math.sin(A + Math.PI / 6), y, z + 0.5 * this.size * Math.cos(A + Math.PI / 6));
+
+        ctx.beginPath();
+        ctx.moveTo(X, Y);
+
+        for(let i=6; i>=3; i--) {
+            const angle = A + (i + 0.5) * Math.PI / 3;
+            const [X, Y] = toIsometricPoint(x + 0.5 * this.size * Math.sin(angle), y - height, z + 0.5 * this.size * Math.cos(angle));
+            ctx.lineTo(X, Y);
+        }
+
+        for(let i=3; i>=0; i--) {
+            const angle = A + (i + 0.5)* Math.PI / 3;
+            const [X, Y] = toIsometricPoint(x + 0.5 * this.size * Math.sin(angle), y, z + 0.5 * this.size * Math.cos(angle));
+            ctx.lineTo(X, Y);
+        }
+        
+        ctx.closePath();
+        
+        ctx.globalAlpha = 1;
+        if(this.glow > 0) {
+            ctx.fillStyle = `hsl(${this.hue}deg ${this.glow}% ${this.glow / 2}%)`;
+            this.glow -= 1;
+        }
+        else {
+            ctx.fillStyle = `hsl(${this.hue}deg 00% 00%)`;
+        }
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.globalAlpha = 0.08;
+        bg.setTransform(this.matrix1);
+        ctx.fillStyle = bg;
+        ctx.fill();
+        ctx.stroke();
+        ctx.globalAlpha = 1;
+
+        ctx.beginPath();
+        ctx.moveTo(X, Y);
+
+        for(let i=0; i<=6; i++) {
+            const angle = A + (i + 0.5) * Math.PI / 3;
+            const [X, Y] = toIsometricPoint(x + 0.5 * this.size * Math.sin(angle), y - height, z + 0.5 * this.size * Math.cos(angle));
+            ctx.lineTo(X, Y);
+        }
+
+        ctx.fillStyle = `hsl(${this.hue}deg 80% 40%)`;
+        if(this.glow > 0) {
+            ctx.fillStyle = `hsl(${this.hue}deg ${this.glow}% ${this.glow / 2}%)`;
+            this.glow -= 1;
+        }
+        else {
+            ctx.fillStyle = `hsl(${this.hue}deg 00% 00%)`;
+        }
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.globalAlpha = 0.12;
+        bg.setTransform(this.matrix2);
+        ctx.fillStyle = bg;
+        ctx.fill();
+        ctx.stroke();
+        ctx.globalAlpha = 1;
+        ctx.restore();
+    }
+
+    update(timestamp) {
+        const v1 = Math.sin(2 * Math.PI * (this.offsetAngle + this.updateSpeed * timestamp));
+        const v2 = Math.cos(2 * Math.PI * (this.offsetAngle + this.updateSpeed * timestamp)) * this.offsetScale;
+        this.offsetHeight = this.offsetScale * this.size * v1;
+        this.matrix1.translateSelf(0, -v2, 0);
+        this.matrix2.translateSelf(v2, -2 * v2, 0);
+
+        if(blocks[0] !== this) {
+            if(v1 > 0 && this.doBeep) {
+                this.beep();
+                this.glow = 100;
+                this.doBeep = false;
+            }
+            
+            if(v1 < 0) {
+                this.doBeep = true;
+            }
+        }
+    }
+}
+
+const canvas = document.getElementById('canvas');
+
+const win = Math.min(window.innerHeight, window.innerWidth);
+const options = {
+    width: win,
+    height: win,
+};
 const width = options.width;
 const height = options.height;
 const center = [options.width / 2, options.height / 2];
@@ -14,46 +174,42 @@ canvas.height = height;
 
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'rgb(0, 255, 0)'
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-function toIsometricPoint(x, y, z) {
-    const X = (x - z) / Math.sqrt(2) + center[0];
-    const Y = (x + 2 * y + z) / Math.sqrt(6) + center[1];
-    //return [x + center[0], z + center[1]]
-    return [X, Y];
-}
+const factor = Math.sqrt(3) / 2;
+const img = document.createElement('img');
+let bg;
 
 
-function drawRegularPolygonPath(n, x, y, z, r) {
-    const angle = Math.PI / n;
-    const [X, Y] = toIsometricPoint(x + 0.5 * r * Math.sin(angle), y, z + 0.5 * r * Math.cos(angle));
-    ctx.moveTo(X, Y);
-    for(let i=1; i<=n; i++) {
-        const angle = i * Math.PI * 2 / n + Math.PI / n;
-        const [X, Y] = toIsometricPoint(x + 0.5 * r * Math.sin(angle), y, z + 0.5 * r * Math.cos(angle));
-        ctx.lineTo(X, Y);
+const blocks = [];
+
+const size = options.width / 10;
+const R = 5;
+for(let s=-R; s<=R; s++) {
+    for(let q=Math.max(-R, -s-R); q<=Math.min(R, -s+R); q++) {
+        blocks.push(new HexBlock(q, 0, s, size, Math.random(), 360 * Math.random()))
     }
 }
 
-const factor = Math.sqrt(3) / 2;
-function hexToPixel(size, q, j, r) {
-    return [size * 1.5 * q, size * j, size * (factor * q + 2 * factor * r)];
+blocks.sort((a, b) => {
+    return a.compare(b);
+});
+
+window.lastBlock = blocks[blocks.length - 1];
+
+function animate(timestamp) {
+    ctx.clearRect(0, 0, options.width, options.height);
+    
+    blocks.forEach((block, i) => {
+        block.render(ctx);
+        block.update(timestamp);
+    });
+
+    requestAnimationFrame(animate);
 }
 
-console.warn([[0, 0, 0], [1, 0, 0], [1, 0, 1], [0, 0, 1]].map(x => toIsometricPoint(...x)));
-
-
 ctx.lineWidth = 2;
-const size = options.width / 4;
+img.onload = function() {
+    bg = ctx.createPattern(img, "repeat")
+    animate(0);
+}
 
-//ctx.translate(0.5, 0.5);
-[[0,0,0], [1, 0, 0], [0, 0, 1], [-1, 0, 0], [0, 0, -1], [1, 0, -1], [-1, 0, 1], [0, -0.5, 0]].forEach(([q, j ,r]) => {
-    let [x, y, z] = hexToPixel(size / 2, q, j, r);    
-    ctx.fillStyle = `hsl(${360 * Math.random()}deg 80% 50%)`;
-    ctx.beginPath();
-    drawRegularPolygonPath(6, x, y, z, size);
-    ctx.fill();
-    ctx.stroke();
-})
-
+img.src = 'bg.png';
